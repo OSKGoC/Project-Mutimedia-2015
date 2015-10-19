c00a81840edfb6b364c198b3a476f544d861cac7
Sakda Jantararoungtong <sakda_jan@hotmail.com>
2015-10-19 13:57:37 +0700

    Last Update BIM
    I just know that this BIM is so hard to reCode

--- Bootstrap Image Gallery/js/demo.js
+++ Bootstrap Image Gallery/js/demo.js
@@ -1,6 +1,6 @@
 /*
- * Bootstrap Image Gallery JS Demo 3.0.1
- * https://github.com/blueimp/Bootstrap-Image-Gallery
+ * blueimp Gallery Demo JS 2.12.1
+ * https://github.com/blueimp/Gallery
  *
  * Copyright 2013, Sebastian Tschan
  * https://blueimp.net
@@ -9,8 +9,7 @@
  * http://www.opensource.org/licenses/MIT
  */
 
-/*jslint unparam: true */
-/*global blueimp, $ */
+/* global blueimp, $ */
 
 $(function () {
     'use strict';
@@ -28,7 +27,8 @@ $(function () {
         dataType: 'jsonp',
         jsonp: 'jsoncallback'
     }).done(function (result) {
-        var linksContainer = $('#links'),
+        var carouselLinks = [],
+            linksContainer = $('#links'),
             baseUrl;
         // Add the demo images as links with thumbnails to the page:
         $.each(result.photos.photo, function (index, photo) {
@@ -40,63 +40,58 @@ $(function () {
                 .prop('title', photo.title)
                 .attr('data-gallery', '')
                 .appendTo(linksContainer);
+            carouselLinks.push({
+                href: baseUrl + '_c.jpg',
+                title: photo.title
+            });
+        });
+        // Initialize the Gallery as image carousel:
+        blueimp.Gallery(carouselLinks, {
+            container: '#blueimp-image-carousel',
+            carousel: true
         });
     });
 
-    $('#borderless-checkbox').on('change', function () {
-        var borderless = $(this).is(':checked');
-        $('#blueimp-gallery').data('useBootstrapModal', !borderless);
-        $('#blueimp-gallery').toggleClass('blueimp-gallery-controls', borderless);
-    });
-
-    $('#fullscreen-checkbox').on('change', function () {
-        $('#blueimp-gallery').data('fullScreen', $(this).is(':checked'));
-    });
-
-    $('#image-gallery-button').on('click', function (event) {
-        event.preventDefault();
-        blueimp.Gallery($('#links a'), $('#blueimp-gallery').data());
-    });
-
-    $('#video-gallery-button').on('click', function (event) {
-        event.preventDefault();
-        blueimp.Gallery([
-            {
-                title: 'Sintel',
-                href: 'https://archive.org/download/Sintel/sintel-2048-surround_512kb.mp4',
-                type: 'video/mp4',
-                poster: 'https://i.imgur.com/MUSw4Zu.jpg'
-            },
-            {
-                title: 'Big Buck Bunny',
-                href: 'https://upload.wikimedia.org/wikipedia/commons/7/75/' +
-                    'Big_Buck_Bunny_Trailer_400p.ogg',
-                type: 'video/ogg',
-                poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/' +
-                    'Big.Buck.Bunny.-.Opening.Screen.png/' +
-                    '800px-Big.Buck.Bunny.-.Opening.Screen.png'
-            },
-            {
-                title: 'Elephants Dream',
-                href: 'https://upload.wikimedia.org/wikipedia/commons/transcoded/8/83/' +
-                    'Elephants_Dream_%28high_quality%29.ogv/' +
-                    'Elephants_Dream_%28high_quality%29.ogv.360p.webm',
-                type: 'video/webm',
-                poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/' +
-                    'Elephants_Dream_s1_proog.jpg/800px-Elephants_Dream_s1_proog.jpg'
-            },
-            {
-                title: 'LES TWINS - An Industry Ahead',
-                type: 'text/html',
-                youtube: 'zi4CIXpx7Bg'
-            },
-            {
-                title: 'KN1GHT - Last Moon',
-                type: 'text/html',
-                vimeo: '73686146',
-                poster: 'https://secure-a.vimeocdn.com/ts/448/835/448835699_960.jpg'
-            }
-        ], $('#blueimp-gallery').data());
+    // Initialize the Gallery as video carousel:
+    blueimp.Gallery([
+        {
+            title: 'Sintel',
+            href: 'https://archive.org/download/Sintel/sintel-2048-surround_512kb.mp4',
+            type: 'video/mp4',
+            poster: 'https://i.imgur.com/MUSw4Zu.jpg'
+        },
+        {
+            title: 'Big Buck Bunny',
+            href: 'https://upload.wikimedia.org/wikipedia/commons/7/75/' +
+                'Big_Buck_Bunny_Trailer_400p.ogg',
+            type: 'video/ogg',
+            poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/' +
+                'Big.Buck.Bunny.-.Opening.Screen.png/' +
+                '800px-Big.Buck.Bunny.-.Opening.Screen.png'
+        },
+        {
+            title: 'Elephants Dream',
+            href: 'https://upload.wikimedia.org/wikipedia/commons/transcoded/8/83/' +
+                'Elephants_Dream_%28high_quality%29.ogv/' +
+                'Elephants_Dream_%28high_quality%29.ogv.360p.webm',
+            type: 'video/webm',
+            poster: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/' +
+                'Elephants_Dream_s1_proog.jpg/800px-Elephants_Dream_s1_proog.jpg'
+        },
+        {
+            title: 'LES TWINS - An Industry Ahead',
+            type: 'text/html',
+            youtube: 'zi4CIXpx7Bg'
+        },
+        {
+            title: 'KN1GHT - Last Moon',
+            type: 'text/html',
+            vimeo: '73686146',
+            poster: 'https://secure-a.vimeocdn.com/ts/448/835/448835699_960.jpg'
+        }
+    ], {
+        container: '#blueimp-video-carousel',
+        carousel: true
     });
 
 });
