eaf4197c85878a84be21ac62b2cef1e6e7a30a78
Sakda Jantararoungtong <sakda_jan@hotmail.com>
2015-11-07 23:50:09 +0700

    Add font , BG , Minor Change

---
+++ prototype/Slider.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<div id="cp_widget_c0b1e059-5512-48d1-bfe9-b6e3416b0a13">...</div><script type="text/javascript">
+var cpo = []; cpo["_object"] ="cp_widget_c0b1e059-5512-48d1-bfe9-b6e3416b0a13"; cpo["_fid"] = "AcPAsANjKzgl";
+var _cpmp = _cpmp || []; _cpmp.push(cpo);
+(function() { var cp = document.createElement("script"); cp.type = "text/javascript";
+cp.async = true; cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
+var c = document.getElementsByTagName("script")[0];
+c.parentNode.insertBefore(cp, c); })(); </script><noscript>Powered by Cincopa <a href='http://www.cincopa.com/video-hosting'>Video Hosting for Business</a> solution.<span>Nice to Meet You</span><span>Studio</span><span>flash</span><span> 16</span><span>cameramake</span><span> Canon</span><span>height</span><span> 533</span><span>orientation</span><span> 1</span><span>camerasoftware</span><span> Adobe Photoshop CS6 </span><span>originaldate</span><span> 5/10/2015 7:28:59 PM</span><span>width</span><span> 800</span><span>cameramodel</span><span> Canon EOS 60D</span><span>flash</span><span> 16</span><span>cameramake</span><span> Canon</span><span>height</span><span> 533</span><span>orientation</span><span> 1</span><span>camerasoftware</span><span> Adobe Photoshop CS6 </span><span>originaldate</span><span> 4/11/2015 4:01:54 PM</span><span>width</span><span> 800</span><span>cameramodel</span><span> Canon EOS 6D</span><span>flash</span><span> 9</span><span>cameramake</span><span> Canon</span><span>height</span><span> 533</span><span>orientation</span><span> 1</span><span>camerasoftware</span><span> Adobe Photoshop CS6 </span><span>originaldate</span><span> 3/28/2015 9:44:43 PM</span><span>width</span><span> 800</span><span>cameramodel</span><span> Canon EOS 60D</span></noscript>
+
+
+</body>
+</html>
